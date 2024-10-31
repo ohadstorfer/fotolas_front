16281b99aab6f148e47b745473642de0b3c5c2eb
ohadstorfer <ohad.storfer@gmail.com>
2024-10-31 13:15:58 -0700

    the freshest222

--- src/components/SessAlbum.tsx
+++ src/components/SessAlbum.tsx
@@ -339,14 +339,16 @@ const SessAlbum: React.FC<SessAlbumProps> = ({ filterType, filterId }) => {
             </ImageListItem>
           ))}
         </ImageList>
-        <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2 }}>
-          <Button variant="contained" onClick={handlePreviousPage} disabled={!previousPage} sx={{ mr: 2 }}>
-            Previous
-          </Button>
-          <Button variant="contained" onClick={handleNextPage} disabled={!nextPage}>
-            Next
-          </Button>
-        </Box>
+        {(nextPage || previousPage) && (
+          <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2 }}>
+            <Button variant="contained" onClick={handlePreviousPage} disabled={!previousPage} sx={{ mr: 2 }}>
+              Previous
+            </Button>
+            <Button variant="contained" onClick={handleNextPage} disabled={!nextPage}>
+              Next
+            </Button>
+          </Box>
+        )}
       </div></>
   );
 };
