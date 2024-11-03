8c6ad3e81c289fb02b255dc86b13e3850eedae8d
ohadstorfer <ohad.storfer@gmail.com>
2024-11-02 19:46:02 -0700

    the freshest139

--- src/components/ResetPassword.tsx
+++ src/components/ResetPassword.tsx
@@ -123,7 +123,7 @@ export default function ResetPassword() {
                 </Button>
                 <Grid container>
                   <Grid item>
-                    <Link href="/sign-up" variant="body2">
+                    <Link href="/SignUp" variant="body2">
                       {"Don't have an account? Sign Up"}
                     </Link>
                   </Grid>
